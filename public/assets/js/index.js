2e225daa6ff6d8daf3570ef395d862c8d819929d
jyc5331 <jcosgrovecoding@gmail.com>
2021-02-02 21:00:33 -0500

    code functioning again. apiroutes and snotes.js communicating properly

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -99,6 +99,7 @@ const handleNoteDelete = (e) => {
 
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
+  console.log("handleNoteView running");
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute("data-note"));
   renderActiveNote();
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,32 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
-const { accessNotes } = require("../../lib/notes");
+const { accessNotes, addNote } = require("../../lib/notes");
 const { v4: uuidv4 } = require("uuid");
 
 router.get("/notes", (req, res) => {
-  accessNotes();
-  const notesArray = JSON.parse(data);
+  const notesArray = accessNotes();
   console.log(notesArray);
   res.json(notesArray);
 });
 
 router.post("/notes", (req, res) => {
+  if (!req.body.title) {
+    return;
+  }
   console.log(req.body);
-  fs.readFile("../../public/notes.html", "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    const notesArray = JSON.parse(data);
-    const newNote = req.body;
-    newNote.id = uuidv4();
-    notesArray.push(req.body);
-    fs.writeFile(dbPath, JSON.stringify(notesArray), (err) => {
-      if (err) throw err;
-      console.log("DB Updated");
-      res.send("File updated");
-    });
-  });
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  const saveResult = addNote(newNote);
+  if (saveResult === true) {
+    res.json(newNote);
+  } else {
+    res.json([]);
+  }
 });
 
 module.exports = router;
